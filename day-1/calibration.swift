aa0eaaf5eabda41985f2caf7d63bb603ad469115
Natik Gadzhi <natik@respawn.io>
2023-12-03 15:00:36 -0800

    Day 1: Move directory structure

--- day-1/calibration.swift
+++ day-1/calibration.swift
@@ -5,18 +5,49 @@
 // Oh gawd string index manipulation in Swift is awkward. 
 // Anyway, run this with `swift calibration.swift`
 
+// String(contentsOfFile:) is in foundation, so we need to require it.
+// We could read the file with fopen and friends, though.
 import Foundation
+
+// Wasteful to make this every function call, so we'll use that in global space.
+let digits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 /// read the input file
 /// and return the array of lines in the file
-func readInputs(inputPath: String) -> [String]? {
-  let input = try? String(contentsOfFile: inputPath)
+func readLines(fromFilePath: String) -> [String]? {
+  let input = try? String(contentsOfFile: fromFilePath)
   guard let input = input else {
     return nil
   }
 
+  // split returns an [StringProtocol], not [String], so we cast them.
   let lines = input.split(separator: "\n").map { String($0) }
   return lines
+}
+
+/// Replaces spelled digits in one String and returns it.
+/// 195one should become 1951
+/// eighthree should become 83
+func replaceSpelledDigits(in input: String) -> String {
+  var result = input
+
+  var replacements = [Range<String.Index>: String]()
+
+  for digit in digits {
+    if let range = input.range(of: digit) {
+      replacements[range] = digit
+    }
+  }
+
+  return result
+}
+
+/// Replaces spelled digits, like 'one', 'two', etc with actual digits in the
+/// array of strings.
+/// 
+/// That's a hightly inefficient way of doing the part 2 of day 1 puzzle ;)
+func replaceSpelledDigits(in inputLines: [String]) -> [String] {
+  return inputLines.map{ replaceSpelledDigits(in: $0) }
 }
 
 /// calibrationValue reads a string and
@@ -56,13 +87,19 @@ func calibrationValue(from input: String) -> UInt8 {
 }
 
 
-guard let lines = readInputs(inputPath: "input.txt") else {
+guard let lines = readLines(fromFilePath: "./input.txt") else {
   print("Could not read input file. Is `input.txt` in the right spot?")
   exit(1)
 }
 
-let calibrationValuesSum = lines.map {
+let inputLines = replaceSpelledDigits(in: lines)
+
+print("Input lines: \(inputLines.count)")
+
+let calibrationValuesSum = inputLines.map {
   calibrationValue(from: $0)
-}.reduce(0, { $0 + Int($1) })
+}.reduce(0) {
+  $0 + Int($1)
+}
 
 print("The sum of all calibration values is \(calibrationValuesSum)")
